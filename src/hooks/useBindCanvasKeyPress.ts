134ae2a45774942dd8904e7e8ee3be01a432adaa
kokodayou2000 <1611601667@qq.com>
2024-12-31 11:11:38 +0800

    style: 修改代码风格

--- src/hooks/useBindCanvasKeyPress.ts
+++ src/hooks/useBindCanvasKeyPress.ts
@@ -1,36 +1,40 @@
 // 绑定快捷键
-import {useKeyPress} from "ahooks";
-import {useDispatch} from "react-redux";
-import {copySelectedComponent, pasteCopiedComponent, removeSelectedComponent} from "@/store/componentsReducer";
-function activeElementValidate(){
-    const activeElement = document.activeElement;
-    if (activeElement === document.body) {
-        // 光标没有 force input 上
-        return true;
-    }
-    return false;
+import { useKeyPress } from 'ahooks';
+import { useDispatch } from 'react-redux';
+import {
+  copySelectedComponent,
+  pasteCopiedComponent,
+  removeSelectedComponent,
+} from '@/store/componentsReducer';
+function activeElementValidate() {
+  const activeElement = document.activeElement;
+  if (activeElement === document.body) {
+    // 光标没有 force input 上
+    return true;
+  }
+  return false;
 }
-function useBindCanvasKeyPress(){
-    const dispatch = useDispatch()
+function useBindCanvasKeyPress() {
+  const dispatch = useDispatch();
 
-    // 删除组件
-    useKeyPress(['backspace','delete'],() => {
-        if (activeElementValidate()){
-            dispatch(removeSelectedComponent())
-        }
-    });
+  // 删除组件
+  useKeyPress(['backspace', 'delete'], () => {
+    if (activeElementValidate()) {
+      dispatch(removeSelectedComponent());
+    }
+  });
 
-    useKeyPress(['ctrl.c','meta.c'],() => {
-        if (activeElementValidate()){
-            dispatch(copySelectedComponent())
-        }
-    });
+  useKeyPress(['ctrl.c', 'meta.c'], () => {
+    if (activeElementValidate()) {
+      dispatch(copySelectedComponent());
+    }
+  });
 
-    useKeyPress(['ctrl.v','meta.v'],() => {
-        if (activeElementValidate()){
-            dispatch(pasteCopiedComponent())
-        }
-    });
+  useKeyPress(['ctrl.v', 'meta.v'], () => {
+    if (activeElementValidate()) {
+      dispatch(pasteCopiedComponent());
+    }
+  });
 }
 
 export default useBindCanvasKeyPress;
